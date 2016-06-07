edbef5ec0300cbd51be3a7d84bbb1de179c86c59
Laurent Peuch <cortex@worlddomination.be>
2016-06-07 14:29:51 +0200

    [enh] first version for cube

--- renew_from_cube.py
+++ renew_from_cube.py
@@ -1,7 +1,9 @@
 import os
 import sys
+import shutil
 from renew import renew
 
+from debug_context_manager import debug
 
 def from_cube():
     if os.path.exists("/etc/openvpn/keys/credentials"):
@@ -12,7 +14,48 @@ def from_cube():
         print("Error: I can't find your credentials for neutrinet since neither /etc/openvpn/keys/credentials nor /etc/openvpn/auth exists on your filesystem")
         sys.exit(1)
 
-    # renew(login, password)
+    result_dir = renew(login, password)
+
+    run_id = result_dir.split("_", 1)[1]
+
+    os.makedirs("/etc/openvpn/keys")
+
+    with debug("Saving old openvpn config"):
+        shutil.copytree("/etc/openvpn/", "/etc/openvpn.old_%s" % run_id)
+
+    with debug("copying new config"):
+        shutil.copy("neutrinet_openvpn_config", "/etc/openvpn/client.conf.tpl")
+
+    os.makedirs("/etc/openvpn/keys")
+
+    with debug("Copying new cert"):
+        shutil.copy(os.path.join(result_dir, "ca.crt"), "/etc/openvpn/keys/ca-server.crt")
+        shutil.copy(os.path.join(result_dir, "client.crt"), "/etc/openvpn/keys/user.crt")
+        shutil.copy(os.path.join(result_dir, "client.key"), "/etc/openvpn/keys/user.key")
+
+    with debug("Adding user credentials"):
+        open("/etc/openvpn/keys/credentials", "w").write("%s\n%s\n" % (login, password))
+
+    commands = [
+        'yunohost app setting vpnclient server_name -v "vpn.neutrinet.be"',
+        'yunohost app setting vpnclient server_port -v "1195"',
+        'yunohost app setting vpnclient server_proto -v "udp"',
+        'yunohost app setting vpnclient service_enabled -v "1"',
+        'yunohost app setting vpnclient login_user -v "%s"' % login,
+        'yunohost app setting vpnclient login_passphrase -v "%s"' % password,
+        'yunohost service add ynh-vpnclient -l /var/log/openvpn-client.log',
+    ]
+
+    for command in commands:
+        with debug("Running command '%s'" % command.replace(password, "xxxxxxxxxxxxxxxxxxxxx")):
+            assert os.systemd(command) == 0, "ERROR: command failed"
+
+    command = "systemctl restart ynh-vpnclient"
+    with debug("Critical part: reloading vpn using '%s'" % command):
+        if os.systemd(command) != 0:
+            print("ERROR: command failed, displaying logs")
+            os.system("cat /var/log/openvpn-client.log")
+            sys.exit(1)
 
 
 if __name__ == '__main__':
